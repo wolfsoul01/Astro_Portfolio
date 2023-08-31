fa56fb80a41ef7b7cc85d80dab81b09fbfc4de23
wolfsoul <alejandrof011021@gmail.com>
2023-08-31 12:40:25 -0400

    Config colors tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor:{
+				"astro-100": '#2A233D',
+				"astro-200": '#4F446D'
+		},
 	},
 	plugins: [],
 }
