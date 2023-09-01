b2203203c31be5849a48cc3b755d54bbda149ffc
wolfsoul <alejandrof011021@gmail.com>
2023-09-01 12:22:31 -0400

    Update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundColor:{
-			"astro-100": '#2A233D',
-			"astro-200": '#4F446D'
-		}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        astro:{
+          100:"#2A233D",
+          200:"#4F446D"
+        }
+      },
+    },
+  },
+  plugins: [],
+};
